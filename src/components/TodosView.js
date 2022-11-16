d75fa8173376d7988564b03edb67e47854672d69
paimdev <lucas@paim.dev>
2022-11-16 18:06:45 -0300

    Create new helper functions and eventListeners

--- src/components/TodosView.js
+++ src/components/TodosView.js
@@ -1,23 +1,91 @@
+import { TodoEditForm } from './TodoEditForm';
+
 export const TodosView = (project) => {
   const todoList = document.createElement('ul');
 
-  project.todos.map((todo) => {
-    const todoItem = document.createElement('li');
+  const clearTodos = () => {
+    todoList.innerHTML = '';
+  };
 
-    const title = todo.title;
-    const dueDate = todo.dueDate;
+  const renderTodos = () => {
+    project.todos.map((todo) => {
+      const todoItem = document.createElement('li');
 
-    const todoTitle = document.createElement('h3');
-    const todoDueDate = document.createElement('p');
+      const id = todo.id;
+      todoItem.id = id;
 
-    todoTitle.innerHTML = title;
-    todoDueDate.innerHTML = dueDate;
+      const title = todo.title;
+      const dueDate = todo.dueDate;
 
-    todoItem.appendChild(todoTitle);
-    todoItem.appendChild(todoDueDate);
+      const todoTitle = document.createElement('h3');
+      const todoDueDate = document.createElement('p');
 
-    todoList.appendChild(todoItem);
-  });
+      todoTitle.innerHTML = title;
+      todoDueDate.innerHTML = dueDate;
+
+      todoItem.appendChild(todoTitle);
+      todoItem.appendChild(todoDueDate);
+
+      todoList.appendChild(todoItem);
+
+      const showExtraInfo = () => {
+        console.log(todo);
+
+        const description = todo.description;
+        const priority = todo.priority;
+        const notes = todo.notes;
+
+        const todoDescription = document.createElement('p');
+        todoDescription.innerHTML = description;
+
+        const todoPriority = document.createElement('p');
+        todoPriority.innerHTML = priority;
+
+        const todoNotes = document.createElement('p');
+        todoNotes.innerHTML = notes;
+
+        todoItem.appendChild(todoDescription);
+        todoItem.appendChild(todoPriority);
+        todoItem.appendChild(todoNotes);
+
+        const editButton = document.createElement('button');
+        editButton.innerHTML = 'Edit';
+        todoItem.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'Delete';
+        todoItem.appendChild(deleteButton);
+
+        const removeTodo = () => {
+          project.removeTodoById(id);
+          clearTodos();
+          renderTodos();
+        };
+
+        const editTodo = () => {
+          removeTodoInfo(todoItem);
+          todoItem.appendChild(TodoEditForm(todo, renderTodos, clearTodos));
+        };
+
+        editButton.addEventListener('click', editTodo);
+        deleteButton.addEventListener('click', removeTodo);
+
+        removeListeners();
+      };
+
+      const removeListeners = () => {
+        todoItem.removeEventListener('click', showExtraInfo);
+      };
+
+      todoItem.addEventListener('click', showExtraInfo);
+    });
+  };
+
+  renderTodos();
+
+  const removeTodoInfo = (todoItem) => {
+    todoItem.innerHTML = '';
+  };
 
   return todoList;
 };
