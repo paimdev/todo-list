1e311278532e2037e30b7473ef32bd873986c7dd
paimdev <lucas@paim.dev>
2022-11-16 12:28:01 -0300

    Create some tests at index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const toDoHolder = (() => {
-  const toDos = [];
-  return { toDos };
-})();
+import { Todo, ProjectsList, Project } from './classes';
 
-console.log(toDoHolder.toDos);
+const mainList = new ProjectsList();
+
+const initTodo = new Todo('test', 'test', 'test', 'test', 'test');
+const initProject = new Project('testProject');
+initProject.addTodo(initTodo);
+mainList.addProject(initProject);
+
+console.log(mainList.getProjects());
