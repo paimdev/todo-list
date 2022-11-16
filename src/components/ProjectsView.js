7fb287a4e475eb234cb36f6156ce0d8093c430df
paimdev <lucas@paim.dev>
2022-11-16 18:06:35 -0300

    Create new helper functions and eventListeners

--- src/components/ProjectsView.js
+++ src/components/ProjectsView.js
@@ -1,24 +1,65 @@
 import { TodosView } from './TodosView';
+import { TodoAddForm } from './TodoAddForm';
+import { ProjectAddForm } from './ProjectAddForm';
 
 export const ProjectsView = (projectsList) => {
   const projectsViewContainer = document.createElement('div');
   const projectsListView = document.createElement('div');
 
-  const projects = projectsList.getProjects();
-  projects.map((project) => {
-    const projectItemContainer = document.createElement('div');
-    const projectItemName = document.createElement('h2');
+  const clearProjects = () => {
+    projectsListView.innerHTML = '';
+  };
 
-    const projectTodosContainer = document.createElement('div');
-    projectTodosContainer.appendChild(TodosView(project));
+  const renderProjects = () => {
+    const projects = projectsList.getProjects();
+    projects.map((project) => {
+      const projectItemContainer = document.createElement('div');
+      const projectItemName = document.createElement('h2');
 
-    projectItemName.innerHTML = project.projectName;
+      const projectTodosContainer = document.createElement('div');
+      projectTodosContainer.appendChild(TodosView(project));
 
-    projectItemContainer.appendChild(projectItemName);
-    projectItemContainer.appendChild(projectTodosContainer);
-    projectsListView.appendChild(projectItemContainer);
-  });
+      projectItemName.innerHTML = project.projectName;
 
+      projectItemContainer.appendChild(projectItemName);
+      projectItemContainer.appendChild(projectTodosContainer);
+      projectsListView.appendChild(projectItemContainer);
+
+      const addTodoButton = document.createElement('button');
+      addTodoButton.innerHTML = 'Add Todo';
+      projectItemContainer.appendChild(addTodoButton);
+
+      const clearProject = () => {
+        projectItemContainer.innerHTML = '';
+      };
+
+      const addTodo = () => {
+        projectItemContainer.appendChild(
+          TodoAddForm(project, clearProjects, renderProjects),
+        );
+      };
+
+      addTodoButton.addEventListener('click', addTodo);
+
+      localStorage.projects = JSON.stringify(projectsList.getProjects());
+    });
+  };
+
+  renderProjects();
   projectsViewContainer.appendChild(projectsListView);
+
+  const addProjectButton = document.createElement('button');
+  addProjectButton.innerHTML = 'Add Project';
+
+  const addProject = () => {
+    projectsViewContainer.appendChild(
+      ProjectAddForm(projectsList, clearProjects, renderProjects),
+    );
+  };
+
+  addProjectButton.addEventListener('click', addProject);
+
+  projectsViewContainer.appendChild(addProjectButton);
+
   return projectsViewContainer;
 };
