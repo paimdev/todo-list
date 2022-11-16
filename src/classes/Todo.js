42ac0ee14c82103dbe651c190b2b7fe4e3778120
paimdev <lucas@paim.dev>
2022-11-16 18:05:26 -0300

    add id to function

--- src/classes/Todo.js
+++ src/classes/Todo.js
@@ -1,5 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class Todo {
-  constructor(title, description, dueDate, priority, notes) {
+  constructor(title, description, dueDate, priority, notes, id = uuidv4()) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+  }
+
+  editTodo(title, description, dueDate, priority, notes) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
