b38f504cd70c1cfbb64d4992dae57e9adb012f44
paimdev <lucas@paim.dev>
2022-11-16 18:05:02 -0300

    Create new tests

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ export const mainList = new ProjectsList();
 
 const initTodo = new Todo(
   'Create new app',
-  'test',
+  'this is a sample description',
   '2022-12-11',
-  'test',
-  'test',
+  'this is a sample note',
+  1,
 );
-const initProject = new Project('testProject');
+
+const otherTodo = new Todo(
+  'Create new app',
+  'this is a sample description',
+  '2022-12-11',
+  'this is a sample note',
+  '1',
+);
+
+const initProject = new Project('Test Project');
 initProject.addTodo(initTodo);
+initProject.addTodo(otherTodo);
 mainList.addProject(initProject);
 
 const contentContainer = document.getElementById('content');
